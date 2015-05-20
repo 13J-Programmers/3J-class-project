4838d2458bb482f3db6d86fe4ed7755f0a7bdce4
荒木誠 <mnfeconicu41@gmail.com>
2015-05-20 19:28:52 +0900

    update KeyAction -- creating DisconnectWithBlock()

--- Assets/Script/KeyAction.cs
+++ Assets/Script/KeyAction.cs
@@ -2,72 +2,75 @@ using UnityEngine;
 using System.Collections;
 
 public class KeyAction : MonoBehaviour {
-	//A container for control target
+	// A container for control target
 	GameObject target;
-	float x = 0.0f,z = 0.0f;
 	BlockController control;
 	public GameObject[] blocks = new GameObject[1];
 	
 	// Use this for initialization
 	void Start () {
-		// GameObject obj = Instantiate(blocks[1]);
-		// obj.name = "block";
-		// ConnectWithBlock();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		x = 0.0f;
-		z = 0.0f;
 		if (!control) {
+			// In future, this method is called by other obj.
 			ConnectWithBlock();
 		}
 
-		//Pitch Block
-		if(Input.GetKeyDown("w")){
+		// Pitch Block
+		if (Input.GetKeyDown("w")) {
 			control.PitchBlock(1);
 		}
-		else if(Input.GetKeyDown("s")){
+		else if (Input.GetKeyDown("s")) {
 			control.PitchBlock(-1);
 		}
 
-		//Yaw Block
-		else if(Input.GetKeyDown("e")){
+		// Yaw Block
+		else if (Input.GetKeyDown("e")) {
 			control.YawBlock(1);
 		}
-		else if(Input.GetKeyDown("q")){
+		else if (Input.GetKeyDown("q")) {
 			control.YawBlock(-1);
 		}
 
-		//Roll Block
-		else if(Input.GetKeyDown("d")){
+		// Roll Block
+		else if (Input.GetKeyDown("d")) {
 			control.RollBlock(1);
 		}
-		else if(Input.GetKeyDown("a")){
+		else if (Input.GetKeyDown("a")) {
 			control.RollBlock(-1);
 		}
 
-		//Move Block
-		if(Input.GetKey("up")){
-			z+=0.1f;
+		// Move Block
+		x = 0.0f;
+		z = 0.0f;
+		if (Input.GetKey("up")) {
+			z = 0.1f;
 			control.MoveBlock(x,z);
 		}
-		else if(Input.GetKey("down")){
-			z+=-0.1f;
+		else if (Input.GetKey("down")) {
+			z = -0.1f;
 			control.MoveBlock(x,z);
 		}
-		else if(Input.GetKey("right")){
-			x+=0.1f;
+		else if (Input.GetKey("right")) {
+			x = 0.1f;
 			control.MoveBlock(x,z);
 		}
-		else if(Input.GetKey("left")){
-			x+=-0.1f;
+		else if (Input.GetKey("left")) {
+			x = -0.1f;
 			control.MoveBlock(x,z);
 		}
 	}
 
 	public void ConnectWithBlock() {
 		target = GameObject.Find("block");
+		control = target.GetComponent<BlockController>();
+	}
+
+	public void DisconnectWithBlock() {
+		target = GameObject.Find("_DummyBlock");
 		control = target.GetComponent<BlockController>();
 	}
 }
