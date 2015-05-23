d51f9ff877beffc6d10ce769fe7ac597160ce096
TeX2e <mnfeconicu41@gmail.com>
2015-05-23 19:09:50 +0900

    create BlockPoolController.cs #11

---
+++ Assets/Script/BlockPoolController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockPoolController : MonoBehaviour {
+	const int POOL_X = 5;
+	const int POOL_Y = 5;
+	const int POOL_Z = 10;
+	int[,,] blockPool = new int[POOL_X, POOL_Y, POOL_Z]; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ControlBlock() {
+
+	}
+
+	// private methods ------------------------------
+
+	private void MergeBlock() {
+
+	}
+
+	private void RemoveCompletedRow() {
+
+	}
+
+	private void FullPool() {
+
+	}
+
+	private void NextPhase() {
+
+	}
+}
