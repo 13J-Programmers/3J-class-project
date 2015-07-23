138cc35ebb411ff1d2ad50cb0fb2ae65d78482c3
Mako <mnfeconicu41@gmail.com>
2015-07-23 11:23:34 +0900

    manage collider size with script

--- Assets/Script/BlockEntity.cs
+++ Assets/Script/BlockEntity.cs
@@ -9,7 +9,17 @@ public class BlockEntity : MonoBehaviour {
 
 	// Use this for initialization
 	void Start() {
-
+		// change every cube of block
+		BoxCollider bc;
+		Vector3 colliderSize = new Vector3(0.8f, 1f, 0.8f);
+		for (int i = 0; i < prefabMaxNum; i++) {
+			bc = blocks[i].GetComponent<BoxCollider>();
+			bc.size = colliderSize;
+			foreach (Transform cube in blocks[i].transform) {
+				bc = cube.gameObject.GetComponent<BoxCollider>();
+				bc.size = colliderSize;
+			}
+		}
 	}
 	
 	// Update is called once per frame
