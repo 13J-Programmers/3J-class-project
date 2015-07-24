8c92dc47fa61391a0e800f7085a6089c0e3b65fb
Mako <mnfeconicu41@gmail.com>
2015-07-24 22:38:52 +0900

    create GameManager.cs

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public int handedness;
+	public int score = 0;
+	public int remainingTime = 180; // sec
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GameStart() {
+
+	}
+
+	public void GameOver() {
+
+	}
+
+	public void GameFinish() {
+		
+	}
+}
