d2d9cbf5ad0a71b5707848c14f48800297b19786
Mako <mnfeconicu41@gmail.com>
2015-07-25 16:58:27 +0900

    create CubeInfo.cs

---
+++ Assets/Script/CubeInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeInfo : MonoBehaviour {
+	private int _score = 0;
+	public int Score {
+		get {
+			return _score;
+		}
+		set {
+			if (value >= 0) return;
+			_score = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
