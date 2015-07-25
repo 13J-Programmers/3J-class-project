6525552bdc68374693683d25f4d9236fa0784196
nobuyo <world_fantasy_2017@live.jp>
2015-07-25 23:32:40 +0900

    add GameInfoViewer

---
+++ Assets/Script/GameInfoViewer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameInfoViewer : MonoBehaviour {
+	public string handedness = "right";
+	public int score = 0;
+	public int remainingTime = 180; // sec
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+	private void OnGUI() {
+
+	}
+
+	private void ShowRemainingTime(Time t) {
+
+	}
+
+	private void ShowNextBlock() {
+
+	}
+
+}
