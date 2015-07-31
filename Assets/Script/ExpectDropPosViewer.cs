1d73bd1e97537842cc0ee7360f1f7c8d622d022b
Mako <mnfeconicu41@gmail.com>
2015-07-31 12:20:18 +0900

    show expected dropping pos (x, y, z)

--- Assets/Script/ExpectDropPosViewer.cs
+++ Assets/Script/ExpectDropPosViewer.cs
@@ -7,16 +7,13 @@ public class ExpectDropPosViewer : MonoBehaviour {
 	int POOL_X;
 	int POOL_Y;
 	int POOL_Z;
-	BlockController blockController;
 	BlockPoolController blockPoolControl;
-	bool[,,] pool;
 	GameObject controllingBlock;
 	GameObject showDropPosBlock;
 	GameObject ground;
 	bool isSync = true;
 
 	void Awake() {
-		blockController = gameObject.GetComponent<BlockController>();
 		blockPoolControl = GameObject.Find("BlockPool").GetComponent<BlockPoolController>();
 		ground = GameObject.Find("BlockPool/Ground");
 		controllingBlock = gameObject;
@@ -24,32 +21,12 @@ public class ExpectDropPosViewer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start() {
-		POOL_X = BlockPoolController.POOL_X;
-		POOL_Y = BlockPoolController.POOL_Y;
-		POOL_Z = BlockPoolController.POOL_Z;
-		pool = new bool[POOL_X, POOL_Y, POOL_Z];
-
-		SetBlockPoolCube();
 		CloneSkeltonBlock();
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		SetBlockPoolCube();
 		SyncOriginBlock();
-
-		// for debug
-		if (Input.GetKeyDown("p")) {
-			for (int z = 0; z < POOL_Z; z++) {
-				for (int y = 0; y < POOL_Y; y++) {
-					string str = "";
-					for (int x = 0; x < POOL_X; x++) {
-						str += (pool[x, y, z] == false) ? "_": "o";
-					}
-					print("(z, y) = (" + z + ", " + y + ") : " + str);
-				}
-			}
-		}
 	}
 
 	public void StopSync() {
@@ -62,17 +39,6 @@ public class ExpectDropPosViewer : MonoBehaviour {
 
 	// private methods --------------------------------
 
-	private void SetBlockPoolCube() {
-		for (int z = 0; z < POOL_Z; z++) {
-			for (int y = 0; y < POOL_Y; y++) {
-				for (int x = 0; x < POOL_X; x++) {
-					if (blockPoolControl.blockPool[x, y, z] == null) continue;
-					pool[x, y, z] = true;
-				}
-			}
-		}
-	}
-
 	// round x,z coordinate
 	private Vector3 roundXZ(Vector3 vector) {
 		Vector3 _vector;
@@ -84,8 +50,11 @@ public class ExpectDropPosViewer : MonoBehaviour {
 
 	// decide position of showDropPosBlock
 	private Vector3 ExpectDropPos(Vector3 position) {
+		// set expected x,z //
 		Vector3 correctedBlockPos = roundXZ(position);
 
+		// set expected y //
+		// get offsets
 		float halfOfWidth = 0.5f;
 		Dictionary<string, float> wallPos = blockPoolControl.GetWallPosition();
 		Vector3 offset = new Vector3(0, 0, 0);
@@ -93,58 +62,55 @@ public class ExpectDropPosViewer : MonoBehaviour {
 		offset.z = -wallPos["z-min"] - halfOfWidth;
 		offset.y = -ground.transform.position.y - halfOfWidth;
 
-		Vector3 height = new Vector3(0, 10f, 0);
+		// set y of controllingBlock
+		int maxPosY = (int)Math.Round(controllingBlock.transform.position.y);
+		Vector3 maxHeight = new Vector3(0, maxPosY, 0);
+
 		var blockCubesPos = new ArrayList();
-		blockCubesPos.Add(roundXZ(controllingBlock.transform.position) - height);
+		blockCubesPos.Add(roundXZ(controllingBlock.transform.position) - maxHeight);
 		foreach (Transform cube in controllingBlock.transform) {
-			blockCubesPos.Add(roundXZ(cube.gameObject.transform.position) - height);
+			blockCubesPos.Add(roundXZ(cube.gameObject.transform.position) - maxHeight);
 		}
 
-		int maxPosY = (int)controllingBlock.transform.position.y;
-		int minPosY = 0;
-		correctedBlockPos.y = maxPosY;
+		// set expected y
+		int height;
+		for (height = maxPosY; height >= 0; height--) {
+			bool isCube = false;
 
-		int i = 0;
+			foreach (Vector3 cubePos in blockCubesPos) {
+				int cubePosX = (int)Math.Round(cubePos.x + offset.x);
+				int cubePosY = (int)Math.Round(cubePos.y) + height;
+				int cubePosZ = (int)Math.Round(cubePos.z + offset.z);
 
-		// need some time to consider
-		/* 
-		for (i = 0; i < maxPosY; i++) {
-			foreach (Vector3 pos in blockCubesPos) {
-				bool isCube = false;
-
-				//print(pos);
-				print(pos.y + i);
-				print(new Vector3((int)(pos.x + offset.x), (int)(i - 1), (int)(pos.z + offset.z)));
-				if (pos.y + i <= 0) {
+				if (cubePosY < 0) {
 					isCube = true;
-				} else if (BlockPoolController.POOL_Y < pos.y + i) {
-					isCube = false;
-				} else {
-					isCube = pool[
-						(int)(pos.x + offset.x), 
-						(int)(pos.y + i - 1), 
-						(int)(pos.z + offset.z)
-					];
+				} else if (BlockPoolController.POOL_Y <= cubePosY) {
+					continue;
+				} else if (!(0 <= cubePosX && cubePosX < BlockPoolController.POOL_X 
+						&& 0 <= cubePosZ && cubePosZ < BlockPoolController.POOL_Z)) {
+					continue;
+				} else if (blockPoolControl.blockPool[cubePosX, cubePosY, cubePosZ] != null) {
+					isCube = true;
 				}
-				
-				if (!isCube) {
-					//print(i + 1);
-					goto LOOP_END;
-				}
+				// print(
+				// 	new Vector3(
+				// 		Math.Round(cubePos.x + offset.x), 
+				// 		cubePosY, 
+				// 		Math.Round(cubePos.z + offset.z)
+				// 	) + " : " + isCube
+				// );
+				if (isCube) break;
+			}
+
+			if (isCube) {
+				break;
+			} else {
+				continue;
 			}
 		}
-		LOOP_END: ;
-		*/
+		height++;
+		correctedBlockPos.y = (float)(height - offset.y);
 
-		correctedBlockPos.y = (float)(i - offset.y);
-
-		/*
-		foreach (Vector3 pos in blockCubesPos) {
-			print(pos);
-		}
-		print("-----");
-		*/
-		
 		return correctedBlockPos;
 	}
 
