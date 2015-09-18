715e987b6d3fc07d0c756476a364c159d55744ff
Mako <mnfeconicu41@gmail.com>
2015-09-18 13:18:26 +0900

    Canvas displayed if 'CanvasController.isGameFinish' flag is true.

---
+++ Assets/Script/CanvasController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasController : MonoBehaviour {
+	public bool isGameFinish = false;
+
+	// Use this for initialization
+	void Start() {
+		gameObject.GetComponent<Canvas>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (isGameFinish) {
+			gameObject.GetComponent<Canvas>().enabled = true;
+		}
+	}
+}
