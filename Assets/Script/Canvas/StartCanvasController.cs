d7d5543b61026f61517f31e9c8bdb39440e73359
Mako <mnfeconicu41@gmail.com>
2015-09-19 12:39:52 +0900

    create Start Canvas

---
+++ Assets/Script/Canvas/StartCanvasController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartCanvasController : MonoBehaviour {
+	private Text countUpText;
+	private Image startImage;
+
+	void Awake() {
+		countUpText = GameObject.Find("StartCanvas/Text - CountUp").GetComponent<Text>();
+		startImage = GameObject.Find("StartCanvas/Image - Start").GetComponent<Image>();
+	}
+
+	// Use this for initialization
+	void Start() {
+		countUpText.enabled = true;
+		startImage.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	public void SetText(string str) {
+		countUpText.text = str;
+	}
+
+	public void SetStart() {
+		SetStart(true);
+	}
+
+	public void SetStart(bool flag) {
+		startImage.enabled = flag;
+	}
+}
