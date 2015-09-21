4cd12e5773b68df42e32e0cc26271308426c4a05
uiby <w1nter.ken@ezweb.ne.jp>
2015-09-21 13:46:56 +0900

    20150921

---
+++ Assets/Title/Script/LeapMotion/HandGesture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// ジェスチャーによる操作(追加の可能性あり)
+/// 1.手を握るとカメラが止まる
+/// </summary>
+public class HandGesture : MonoBehaviour {
+    private CameraCircularMotion mainCameraMotion;
+    private CameraCircularMotion secondCameraMotion;
+    
+    void Start () {
+        mainCameraMotion = GameObject.Find("MainCamera").GetComponent<CameraCircularMotion>();
+        secondCameraMotion = GameObject.Find("SecondCamera").GetComponent<CameraCircularMotion>();  
+	}
+	
+	void Update () {
+    }
+    
+    public void cameraStop(bool flag){
+        mainCameraMotion.enabled = flag;
+        secondCameraMotion.enabled = flag;
+    }
+}
