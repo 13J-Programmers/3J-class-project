23b6b7f5c2d68fca318c0e9372010ee67ebffead
Mako <mnfeconicu41@gmail.com>
2015-09-24 20:48:26 +0900

    create Wall class

--- Assets/Script/BlockPool/Wall.cs
+++ Assets/Script/BlockPool/Wall.cs
@@ -3,9 +3,24 @@ using System;
 using System.Collections;
 
 public class Wall : MonoBehaviour {
+	Range<float> xRange;
+	Range<float> zRange;
+
+	public float GetMinX() { return xRange.GetMin(); }
+	public float GetMaxX() { return xRange.GetMax(); }
+	public float GetMinZ() { return zRange.GetMin(); }
+	public float GetMaxZ() { return zRange.GetMax(); }
+	public GameObject GetWall(string path) {
+		return GameObject.Find("BlockPool/Walls/" + path);
+	}
+
 	void Start() {
-		// Range<float> xRange = new Range<float>(0.1f, 0.9f);
-		// print(xRange);
+		float minX = GetWall("min-x").transform.position.x;
+		float maxX = GetWall("max-x").transform.position.x;
+		float minZ = GetWall("min-z").transform.position.z;
+		float maxZ = GetWall("max-z").transform.position.z;
+		xRange = new Range<float>(minX, maxX);
+		zRange = new Range<float>(minZ, maxZ);
 	}
 }
 
