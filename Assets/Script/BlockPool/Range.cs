d4f697b5d6b461e86e8c5bb52c702a14b6330beb
Mako <mnfeconicu41@gmail.com>
2015-09-25 11:59:50 +0900

    extract Range class

---
+++ Assets/Script/BlockPool/Range.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Range<T> where T : IComparable {
+	private T min;
+	private T max;
+
+	public T GetMin() { return min; }
+	public T GetMax() { return max; }
+	private void SetMin(T min) { this.min = min; }
+	private void SetMax(T max) { this.max = max; }
+
+	public Range(T min, T max) {
+		if (min.CompareTo(max) > 0) throw new RangeException();
+		this.min = min;
+		this.max = max;
+	}
+
+	override
+	public string ToString() {
+		return "(" + min + ".." + max + ")";
+	}
+	
+	public bool Include(T val) {
+		return (min.CompareTo(val) <= 0) && (val.CompareTo(max) < 0);
+	}
+}
+
+public class RangeException : Exception {
+	public RangeException() : base() {}
+	public RangeException(string message) : base(message) {}
+	public RangeException(string message, Exception inner) : base(message, inner) {}
+}
+
--- Assets/Script/BlockPool/Wall.cs
+++ Assets/Script/BlockPool/Wall.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections;
 
 public class Wall : MonoBehaviour {
@@ -24,33 +23,3 @@ public class Wall : MonoBehaviour {
 	}
 }
 
-public class Range<T> where T : IComparable {
-	private T min;
-	private T max;
-
-	public T GetMin() { return min; }
-	public T GetMax() { return max; }
-	private void SetMin(T min) { this.min = min; }
-	private void SetMax(T max) { this.max = max; }
-
-	public Range(T min, T max) {
-		if (min.CompareTo(max) > 0) throw new RangeException();
-		this.SetMin(min);
-		this.SetMax(max);
-	}
-
-	override
-	public string ToString() {
-		return "(" + min + ".." + max + ")";
-	}
-	
-	public bool Include(T val) {
-		return (min.CompareTo(val) < 0) && (val.CompareTo(max) < 0);
-	}
-}
-
-public class RangeException : Exception {
-	public RangeException() : base() {}
-	public RangeException(string message) : base(message) {}
-	public RangeException(string message, Exception inner) : base(message, inner) {}
-}
