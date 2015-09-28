5bdbd8200eef6690b99b22279eb93d14388727da
Mako <mnfeconicu41@gmail.com>
2015-09-28 10:35:34 +0900

    sync sub camera with main

---
+++ Assets/Script/SubCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubCameraController : MonoBehaviour {
+	private GameObject GetMainCameraObj() {
+		return GameObject.Find("Main Camera");
+	}
+	
+	void Update() {
+		// angle
+		this.gameObject.transform.localEulerAngles = 
+			GetMainCameraObj().transform.eulerAngles + 
+			Vector3.right * -30;
+
+		// position
+		this.gameObject.transform.position = 
+			GetMainCameraObj().transform.position + 
+			Vector3.up * 90 + 
+			this.gameObject.transform.forward * 5;
+	}
+}
