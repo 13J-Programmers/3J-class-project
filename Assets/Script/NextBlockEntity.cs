c51fee9bc3e37d0842c19d49e48e9d0cb7a12f05
Mako <mnfeconicu41@gmail.com>
2015-09-28 11:32:34 +0900

    set name to next block

--- Assets/Script/NextBlockEntity.cs
+++ Assets/Script/NextBlockEntity.cs
@@ -17,13 +17,15 @@ public class NextBlockEntity : MonoBehaviour {
 	private void InstantiateBlock(object sender, EventArgs e) {
 		InstantiateBlock(GetNextBlockObj());
 	}
-	
+
 	private void InstantiateBlock(GameObject block) {
 		nextBlockObj = Instantiate(
 			block, // instance object
 			new Vector3(0, 100, 0), // coordinate
 			block.transform.rotation // rotation
 		) as GameObject;
+
+		nextBlockObj.name = "nextBlock";
 	}
 
 	private void DestoryBlock(object sender, EventArgs e) {
