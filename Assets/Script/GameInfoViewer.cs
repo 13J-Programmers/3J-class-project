454054f7ce9b83134e52543deae5a979b7cd0601
Mako <mnfeconicu41@gmail.com>
2015-10-06 21:29:17 +0900

    put large string such as score and times

--- Assets/Script/GameInfoViewer.cs
+++ Assets/Script/GameInfoViewer.cs
@@ -20,9 +20,9 @@ public class GameInfoViewer : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		guiStyle = new GUIStyle();
-		guiStyle.fontSize = 16;
+		guiStyle.fontSize = 30;
 		guiStyleState = new GUIStyleState();
-		guiStyleState.textColor = Color.white;
+		guiStyleState.textColor = Color.black;//Color.white;
 		guiStyle.normal = guiStyleState;
 	}
 
@@ -31,14 +31,14 @@ public class GameInfoViewer : MonoBehaviour {
 	///   * score
 	///   * game time
 	private void OnGUI() {
-		GUI.Label(new Rect(10, Screen.height - 60, 100, 60), "Lines : " + gameManager.lines, guiStyle);
-		GUI.Label(new Rect(10, Screen.height - 40, 100, 60), "Score : " + gameManager.score, guiStyle);
+		GUI.Label(new Rect(10, Screen.height - guiStyle.fontSize * 3.4f, 100, 60), "Lines : " + gameManager.lines, guiStyle);
+		GUI.Label(new Rect(10, Screen.height - guiStyle.fontSize * 2.2f, 100, 60), "Score : " + gameManager.score, guiStyle);
 		int timeLeft = (int)gameManager.remainingTime;
 		int min = timeLeft / 60;
 		int sec = timeLeft % 60;
 		string time = "Time Left : ";
 		if (min != 0) time += min + "min ";
 		time += sec + "sec";
-		GUI.Label(new Rect(10, Screen.height - 20, 100, 60), time, guiStyle);
+		GUI.Label(new Rect(10, Screen.height - guiStyle.fontSize * 1, 100, 60), time, guiStyle);
 	}
 }
