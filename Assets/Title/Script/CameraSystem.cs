4fcbd79b6fa3825d9e064e3e93517d3a2e25d0dc
uiby <w1nter.ken@ezweb.ne.jp>
2015-10-11 19:29:19 +0900

    For title scene . Change the camera in one . Add a gesture by both hands .

---
+++ Assets/Title/Script/CameraSystem.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSystem  : MonoBehaviour {
+	public GameObject target;
+	public float rad; // 半径
+	public float hight; // 高さ
+	public float angle; // ラジアン値
+	public float speed;
+	private bool circularMotionFlag;
+	// Use this for initialization
+	void Start() {
+		circularMotionFlag = true;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (circularMotionFlag)
+		{
+			float time = Time.deltaTime;
+			Vector3 CameraPos = target.transform.position;
+			// オブジェクトの周りをカメラが円運動する
+			transform.position = new Vector3(
+				CameraPos.x + Mathf.Cos(angle) * rad,
+				CameraPos.y + hight,
+				CameraPos.z + Mathf.Sin(angle) * rad
+			);
+			angle += speed * time;
+			//print("MainCamera x:" + this.transform.position.x);
+			//print("MainCamera y:" + this.transform.position.y);
+			//print("MainCamera z:" + this.transform.position.z);
+			transform.LookAt(CameraPos); // カメラをtargetへ向かせる
+		}
+	}
+	public void StopOrMove(bool moveFlag)//カメラを停止または動かす
+	{
+		circularMotionFlag = moveFlag;
+	}
+	public void MoveUpOrDown(int moveMode)//カメラを上または下に移動
+	{
+		Animator animator = GetComponent<Animator>();
+		switch (moveMode)
+		{
+			case 1://FromTopToBottom
+				if (animator.GetInteger("MoveMode") == 2 || animator.GetInteger("MoveMode") == 0)
+					animator.SetInteger("MoveMode", moveMode);
+				break;
+			case 2://FromBottomToTop
+				if (animator.GetInteger("MoveMode") == 1 || animator.GetInteger("MoveMode") == 0)
+					animator.SetInteger("MoveMode", moveMode);
+				break;
+		}
+	}
+}
--- Assets/Title/Script/LeapMotion/HandControl.cs
+++ Assets/Title/Script/LeapMotion/HandControl.cs
@@ -4,20 +4,25 @@ using Leap;
 
 /// <summary>
 /// LeapMotionで手のひらを感知
+/// ジェスチャーによる操作
+/// 1.片手で握るとカメラが止まる
+/// 2.両手で広げる、または閉じるとカメラの位置が変わる(速度で判断)
 /// </summary>
 public class HandControl : MonoBehaviour {
-	private HandGesture handGesture;
 	private Controller controller;
 	private InteractionBox interactionBox;
 	private Frame frame;
-	public int debugPlamCount;//手のひらの個数
+	private CameraSystem cameraSystem;
+	public int plamCount;
 	public int debugFingerCount;
 	public float x, y, z;
-	public GameObject[] fingerObject;
-
-	void Start() {
-		handGesture = GetComponent<HandGesture>();
+	private GameObject plam;
+	private bool existPlamFlag;
+	void Start()
+	{
+		cameraSystem = GameObject.Find("MainCamera").GetComponent<CameraSystem>();  
 		controller = new Controller();
+		existPlamFlag = false;
 	}
 
 	void Update()
@@ -25,28 +30,70 @@ public class HandControl : MonoBehaviour {
 		frame = controller.Frame();
 		interactionBox = frame.InteractionBox;
 		Hand hand = frame.Hands[0];
-		debugPlamCount = frame.Hands.Count;
+		plamCount = frame.Hands.Count;
 		debugFingerCount = frame.Fingers.Count;
-		this.GetComponent<Renderer>().enabled = hand.IsValid;
-		this.GetComponent<Collider>().enabled = hand.IsValid;
 		Vector plamPos = interactionBox.NormalizePoint(hand.PalmPosition);
-		int a = 4;//感知する領域を広げる //< 一文字の変数は使わないこと。代わりに scale など
-		x = a * (plamPos.x - 0.5f);
-		y = a * plamPos.y;
-		z = a * (plamPos.z - 0.5f);
-		Vector p = new Vector(x, y, z); //< 一文字の変数は使わないこと。代わりに pos など
-		this.transform.localPosition = ToVector3(p);
-		// >>>> ここから
-		Vector3 pos = this.transform.localPosition;
-		pos.z = -pos.z;
-		this.transform.localPosition = pos;
-		// <<<< ここまでの処理は、6行上のコードを new Vector(x, y, -z) とするだけで十分かも？ (mako)
-		if (hand.GrabStrength > 0.6f)
-			handGesture.cameraStop(false);
-		else
-			handGesture.cameraStop(true);
+		int scale = 4;//感知する領域を広げる
+		x = scale * (plamPos.x - 0.5f);
+		y = scale * plamPos.y;
+		z = scale * (plamPos.z - 0.5f);
+		if (plamCount <= 1)//片手の場合
+		{
+			if (hand.IsValid == false && existPlamFlag)
+			{
+				existPlamFlag = false;
+				Destroy(plam);
+			}
+			if (!existPlamFlag)
+			{
+				if (hand.IsValid)
+				{
+					existPlamFlag = true;
+					plam = (GameObject)Instantiate(Resources.Load("Title/Plam"), ToVector3(new Vector(x, y, -z)), Quaternion.identity);
+					plam.transform.SetParent(GameObject.Find("MainCamera/LeapPos").transform);
+					plam.GetComponent<Renderer>().enabled = hand.IsValid;
+					plam.GetComponent<Collider>().enabled = hand.IsValid;
+				}
+			}
+			else
+			{
+				plam.transform.localPosition = ToVector3(new Vector(x, y, -z));
+			}
+			HoldDecision(hand);
+		}
+		else if (plamCount >= 2)//両手の場合
+		{
+			existPlamFlag = false;
+			Destroy(plam);
+			BothHandsGesture();
+		}
 	}
-
+	private void HoldDecision(Hand hand)//握ったかどうかの判断
+	{
+		if (hand.GrabStrength > 0.6f)
+			cameraSystem.StopOrMove(false);
+		else
+			cameraSystem.StopOrMove(true);
+	}
+	private void BothHandsGesture()//両手のジェスチャーの判断
+	{
+		Hand leftMost = frame.Hands.Leftmost;
+		Hand rightMost = frame.Hands.Rightmost;
+		if (leftMost.Id != rightMost.Id)
+		{
+			//Debug.Log(leftMost.PalmVelocity + ":" + rightMost.PalmVelocity);
+			if (leftMost.PalmVelocity.x <= -500 && rightMost.PalmVelocity.x >= 500)//|5*10^-3|m/s到達時//拡大
+			{
+				//Debug.Log("カメラ移動(下から上)" + leftMost.PalmVelocity.x);
+				cameraSystem.MoveUpOrDown(1);
+			}
+			else if (leftMost.PalmVelocity.x >= 300 && rightMost.PalmVelocity.x <= -300)//|3*10^-3|m/s到達時//縮小
+			{
+				//Debug.Log("Okカメラ移動(上から下)" + leftMost.PalmVelocity.x);
+				cameraSystem.MoveUpOrDown(2);
+			}
+		}
+	}
 	Vector3 ToVector3(Vector v)//座標の変換
 	{
 		return new UnityEngine.Vector3(v.x, v.y, v.z);
