5dea30bee07930afa79b0ded3d22ee6951b96c7a
Mako <mnfeconicu41@gmail.com>
2015-10-13 18:21:18 +0900

    create Test class, which provides function "test", as predicate.

---
+++ Assets/Script/lib/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Test {
+	static public bool test(Func<bool> predicate) {
+		try {
+			return predicate();
+		} catch (Exception) {
+			return false;
+		}
+	}
+}
