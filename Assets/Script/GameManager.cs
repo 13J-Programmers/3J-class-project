be7aa7f75bdb059cb541960457ca3514d20a606c
Mako <mnfeconicu41@gmail.com>
2015-10-19 16:45:31 +0900

    move ComponentInfo class

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -253,22 +253,3 @@ public class GameManager : MonoBehaviour {
 }
 
 
-/// instance has a componentName and attachedObjName
-public class ComponentInfo {
-	public string attachedObjName;
-	public string componentName;
-
-	public ComponentInfo(string attachedObjName, string componentName) {
-		this.attachedObjName = attachedObjName;
-		this.componentName = componentName;
-	}
-
-	override
-	public string ToString() {
-		return attachedObjName + "#" + componentName;
-	}
-
-	public Component GetComponent() {
-		return GameObject.Find(attachedObjName).GetComponent(componentName);
-	}
-}
---
+++ Assets/Script/lib/ComponentInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// instance has a componentName and attachedObjName
+public class ComponentInfo {
+	public string attachedObjName;
+	public string componentName;
+
+	public ComponentInfo(string attachedObjName, string componentName) {
+		this.attachedObjName = attachedObjName;
+		this.componentName = componentName;
+	}
+
+	override
+	public string ToString() {
+		return attachedObjName + "#" + componentName;
+	}
+
+	public Component GetComponent() {
+		return GameObject.Find(attachedObjName).GetComponent(componentName);
+	}
+}
